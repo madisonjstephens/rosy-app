bacc13ef53dd3317eedf90833fb3bf8e10bbee23
Kelvin Fichter <kef50@case.edu>
2016-10-09 05:37:56 -0400

    eml

--- rosy-interface/src/pages/profile/profile.ts
+++ rosy-interface/src/pages/profile/profile.ts
@@ -7,6 +7,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'profile.html'
 })
 export class ProfilePage {
+    private userName = ''
+    private name;
+    private email;
+    private wh;
+    private journal;
+    private date;
+    private xtra;
     private dropdowns: any[] = [
         {
             title: 'profile settings',
@@ -30,10 +37,26 @@ export class ProfilePage {
     ];
     
     constructor(public navCtrl: NavController) {
-
+        window['emailjs'].init("user_1Ir7hNIXcBASJvvUBWtK6");
+        console.log(window['emailjs']);
     }
     
     toggleDropdown(dropdown) {
         dropdown.open = !dropdown.open;
+    }
+    
+    sendEmail(dropdown) {
+        window['emailjs'].send('default_service', 'template_oNkkRwDS', {
+            "from": "Della Carter",
+            "from_email": "dcarter@gmail.com",
+            "date": this.date,
+            "to": this.name,
+            "to_email": this.email
+        }).then(function(response) {
+            dropdown.open = false;
+            console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
+        }, function(err) {
+            console.log("FAILED. error=", err);
+        });
     }
 }
